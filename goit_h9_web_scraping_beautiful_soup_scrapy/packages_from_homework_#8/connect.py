4c59ba7e4f4ff994ee08ea8473897d7da8a96ac5
Paweł Szumański <pawel.szumanski@tuta.io>
2024-02-28 10:26:40 +0100

    Add essential packages from homework 8

---
+++ goit_h9_web_scraping_beautiful_soup_scrapy/packages_from_homework_#8/connect.py
@@ -0,0 +1,26 @@
+import sys
+from pathlib import Path
+
+from mongoengine import connect
+from pymongo.errors import ConfigurationError
+import configparser
+
+config = configparser.ConfigParser()
+config_path = "config_dev.ini"
+config.read(config_path)
+
+mongo_user = config.get("DB", "user")
+mongodb_pass = config.get("DB", "pass")
+db_name = config.get("DB", "db_name")
+domain = config.get("DB", "domain")
+
+try:
+    # connect to cluster on AtlasDB with connection string
+    connect(
+        host=f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority""",
+        ssl=True,
+    )
+except ConfigurationError as e:
+    print("Problem with database connection, check your connection and credentials settings.")
+    print(f"Error: {e}")
+    sys.exit(1)
